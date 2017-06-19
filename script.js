756793f9d8df37d391b705af7e44dcd2b5944dd1
ondrarethy <ondrarethy@gmail.com>
2017-06-19 09:22:57 +0200

    rethy_web
    Personal website

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+var scrollEventHandler = function()
+{
+  window.scroll(0, window.pageYOffset)
+}
+
+window.addEventListener("scroll", scrollEventHandler, false);
+
+function bindEvent(e, eventName, callback) {
+    if(e.addEventListener) // new browsers
+        e.addEventListener(eventName, callback, false);
+    else if(e.attachEvent) // IE
+        e.attachEvent('on'+ eventName, callback);
+};
+
+bindEvent(document.body, 'scroll', function(e) {
+    document.body.scrollLeft = 0;
+});
